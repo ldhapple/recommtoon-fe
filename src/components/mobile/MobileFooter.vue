0e7b853b5f95abfdddde7a5bc3a95bcc066d6fea
leedohyun <lucky77795@naver.com>
2024-02-29 18:00:24 +0900

    feat: MobileFooter, MobileHeader, PcHeader 추가
    모바일과 PC의 메뉴 및 로그인 버튼에 대한 접근에 차이를 두기 위해 생성

---
+++ src/components/mobile/MobileFooter.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'MobileFooter',
+  data() {
+    return {
+      isMenuVisible: false,
+      activeMenu: '',
+      menus: [
+        {name: 'Features', path: '/features'},
+        {name: 'Pricing', path: '/pricing'},
+        {name: 'FAQs', path: '/faqs'},
+        {name: 'About', path: '/about'},
+      ],
+    };
+  },
+  methods: {
+    setActiveMenu(menuName) {
+      this.activeMenu = menuName;
+      this.isMenuVisible = false; // Close the navbar-collapse on click
+    }
+  }
+}
+</script>
+
+<template>
+  <footer class="d-flex justify-content-center py-3">
+    <ul class="nav nav-pills">
+      <li v-for="menu in menus" :key="menu.name" class="nav-item">
+        <router-link :to="menu.path" class="nav-link" :class="{ 'active': activeMenu === menu.name }"
+                     @click="setActiveMenu(menu.name)">
+          {{ menu.name }}
+        </router-link>
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/pc/PcHeader.vue
+++ src/components/pc/PcHeader.vue
@@ -29,8 +29,7 @@
 
 <script>
 export default {
-  // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Header',
+  name: 'PcHeader',
   data() {
     return {
       isMenuVisible: false,
