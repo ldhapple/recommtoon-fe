d5dd75a4e75a51432d2ea5931dde8833ebb1e842
leedohyun <lucky77795@naver.com>
2024-02-29 17:13:40 +0900

    feat: footer 추가

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <footer class="py-3 my-4">
+      <p class="text-center text-body-secondary">&copy; 2024 Recommtoon</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Footer'
+}
+</script>
+
+<style scoped/>
